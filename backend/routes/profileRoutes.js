23ca06824468d8372cf72e5c8e247f32606cf66c
Yash Suthar <yashsuthar0309@gmail.com>
2025-01-29 17:47:15 +0530

    removed extra files

--- backend/routes/profileRoutes.js
+++
@@ -1,9 +0,0 @@
-const express  = require('express')
-const router = express.Router()
-const { getProfile, updateProfile } = require("../controllers/profileController")
-const validateToken = require('../middleware/validateTokenHandler')
-
-router.use(validateToken)
-router.route("/:id").get(getProfile).put(updateProfile)
-
-module.exports = router
